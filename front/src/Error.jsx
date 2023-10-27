25ff2883b45ef1333d4c828b8fbdc3a5480e5baf
lucdaCook <lucacaputodev@gmail.com>
2023-10-27 15:39:25 -0700

    cleanup

--- front/src/Error.jsx
+++ front/src/Error.jsx
@@ -1,6 +1,4 @@
 import { Link, useLocation } from "react-router-dom";
-import { RainyCloud } from "./svg/clouds";
-import MainView from './components/MainView'
 export default function ErrorPage({ notFound }) {
 
   const state = useLocation().state
@@ -8,31 +6,11 @@ export default function ErrorPage({ notFound }) {
   console.log(notFound)
   
   return (
-    <>
-    {/* <MainView />  This here is optional*/}
       <div className="error">
-        { notFound ? 
-        <>
-          <div className="not-found clouds" >
-              <RainyCloud />
-              <RainyCloud />
-              <RainyCloud />
-          </div>
-          <div className="info">
-          <span>Was that a typo?</span>
-          <Link to='/'>Go back to home</Link>
-          </div>
-        </>
-        :
-        <>
-          <RainyCloud />
-          <div className="info">
-            <span>Oops...That didn't work for some reason.</span>
-            <Link to={state?.goTo? state.goTo : state?.from ? state.from : '/'} state={{'from': '/'}}>{state?.message ? state.message : 'Maybe try again?'}</Link>
-          </div>
-        </>
-      }
+        <div className="info">
+          <span>Oops...That didn't work for some reason.</span>
+          <Link to={state?.goTo? state.goTo : state?.from ? state.from : '/'} state={{'from': '/'}}>{state?.message ? state.message : 'Maybe try again?'}</Link>
+        </div>
       </div>
-    </>
   )
 }
