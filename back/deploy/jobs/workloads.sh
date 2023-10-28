5fceeaf05e62bc64b0a9083fc9e50689d13b583e
lucdaCook <lucacaputodev@gmail.com>
2023-10-28 12:15:29 -0700

    compose

--- back/deploy/jobs/workloads.sh
+++
@@ -1,7 +0,0 @@
-# export PYTHONPATH=$PYTHONPATH:$PWD
-# mkdir workflow
-
-# export TESTS_RESULTS_FILE=workflow/test_results.txt
-# python -m pytest tests/scripts --verbose --disable_warnings > TESTS_RESULTS_FILE
-# cat TESTS_RESULTS_FILE
-
